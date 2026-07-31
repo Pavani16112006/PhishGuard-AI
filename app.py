6b6fb8d483dcb48f517998c2384ffb4421b17d6a
Pavani16112006 <pavanisirivalya@gmail.com>
2026-07-31 19:10:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from ai import analyze
 
 app = FastAPI()
 
-class Site(BaseModel):
-    url:str
-    title:str
-    text:str
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Website(BaseModel):
+    url: str
+    title: str
+    text: str
 
 @app.post("/analyze")
-def analyze_site(site:Site):
-    return analyze(site)
+def analyze_website(site: Website):
+    result = analyze(site)
+    return {"result": result}
